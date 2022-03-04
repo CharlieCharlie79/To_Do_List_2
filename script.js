08e9d2aeaaec9979234d3aea482c794fbff5b729
CharlieCharlie79 <carloswork067@hotmail.com>
2022-03-04 02:44:59 -0500

    The delete button
    It needs to be fixed but gladly is it close to be finished.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const addItem = document.querySelector(".add-item");
-const removeItem = document.querySelector(".eliminate-item");
 const ulList = document.querySelector(".ul-list");
 
 
 
+// adds item
 addItem.addEventListener('click', function()
 {
     //gets the user input
@@ -14,7 +14,69 @@ addItem.addEventListener('click', function()
     }
 
     //creates the li element
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode(getVal()));
-    ulList.appendChild(li);
+    function createLi()
+    {
+        var li = document.createElement('li');
+        //lets give him an id
+        li.setAttribute('id', "liItem");
+        li.appendChild(document.createTextNode(getVal()));
+        ulList.appendChild(li);
+
+        //lets add the incon
+        document.querySelector('.ul-list').innerHTML += ` 
+        <button class="delete"> 
+        <i class= "fa-solid fa-trash-can" onclick="deleteButton()"> </i> 
+        </button>`;
+    }
+    
+    //Checks if input is empty
+    if(!getVal())
+    {
+        alert("You need to put a task!")
+        return false;
+    }
+    else
+    {
+        createLi();   
+    }
+
 })
+
+function deleteButton()
+{ 
+    /*
+   let ulvar = document.getElementById('ul-id');
+   ulvar.removeChild(ulvar.lastElementChild);*/
+
+
+   
+   //var listItems = document.getElementsByTagName('li');
+   var listItems =document.querySelectorAll("li");
+   for(var i = 0; i < listItems.length; i++)
+   {
+       listItems[i].onclick = function()
+       {
+           this.parentNode.removeChild(this);
+       }
+   }
+   
+   console.log('Item removed');
+}
+
+//remove  item
+//If I click on the trash icon I active the delete function
+/*
+deleteButton.addEventListener('click', function()
+{
+    ulList.onclick = function(event)
+    {
+        var target = event.target;
+        console.log(target.innerText);
+        
+        //Removes item
+        target.remove(target.selectedIndex);
+    }
+})
+*/
+
+
